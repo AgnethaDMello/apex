29b7d2a77fa7312cdeaca8114d3501c261cb895c
AgnethaDMello <agnethad@nycstudents.net>
2022-07-20 14:39:09 -0400

    progress i think

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,47 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      
+<img id="logo" src="/images/apex.png" alt="Apex Enterprises Logo"/>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">Services</RouterLink>
+        <RouterLink to="/contact">Contact Us</RouterLink>
+      </nav>
+      
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView/>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.wrapper{
+  display: flex;
+  padding: 1rem;
+  background-color: var(--dark-brown);
+  width: 100%;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#logo{
+  justify-content: center;
+  width: 10%;
+  margin-right: 10%;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+nav{
+  width: 100%;
+  margin-top: 0.8%;
 }
 </style>
---
+++ website/src/views/ContactView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="contact">
+    <h1>Contact Us</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .contact {
+    color: white;
+  }
+}
+</style>
